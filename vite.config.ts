df6fe5648baa65575f1b0b0a20ee5f81d1c8575a
Tomas Ellis <tomas.sydney.ellis@gmail.com>
2025-08-09 16:08:06 -0300

    misc modules fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import path from 'path'
 
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+dotenv.config({ path: '../.env' }); // Adjust path as needed
+
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:3010'}`;
+  const PORT = `${env.VITE_API_PORT ?? '3010'}`;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
+});
